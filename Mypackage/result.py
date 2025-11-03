aa8815bb5110fe959f3434cf7147ab09b17805f4
ahamedshadi1234-alt <ahamedshadi1234@gmail.com>
2025-11-03 15:54:37 +0530

    Add files via upload

---
+++ Mypackage/result.py
@@ -0,0 +1,8 @@
+from Mypackage import student
+from Mypackage import staff
+
+st=student.total_students()
+print("The total student count is : ",st)
+
+sf=staff.total_staff()
+print("The total staff count is : ",sf)
---
+++ Mypackage/student.py
@@ -0,0 +1,2 @@
+def total_students():
+     return 1000
