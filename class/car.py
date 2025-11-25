e1fa49686a627007a1b02e2b8c90e63e86ba77c4
ahamedshadi1234-alt <ahamedshadi1234@gmail.com>
2025-11-25 15:14:37 +0530

    Add files via upload

---
+++ class/car.py
@@ -0,0 +1,5 @@
+class car:
+    def start-engine(self):
+        print("enginme started")
+    my car=car()
+    mycar.start_engine()
---
+++ class/car1.py
@@ -0,0 +1,10 @@
+class car:
+    def__init__(self,brand,color):
+        self.brand=brand
+        self.color=color
+    def display(self):
+        print("Myu car is a ",self.color,self.brand)
+c1=car("Toyota","Red")
+c2=car("Honda","blue")_
+c1.display()
+c2.display()
